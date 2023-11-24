d7ae726ee2a1a00eeaec9603e9b919da46359d2e
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-11-24 18:08:24 -0500

    clean code to make it readable.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
+import axios from "axios";
+import {useEffect, useState} from "react";
 import AmountInput from "./AmountInput";
+import LoadingSkeleton from "./LoadingSkeleton";
 import ResultRow from "./ResultRow";
-import {sortBy} from  'lodash';
+import {sortBy} from 'lodash';
 import  useDebouncedEffect  from  'use-debounced-effect';
 
-
 type CachedResult = {
-  provider:string;
-  btc:string;
-}
+  provider: string;
+  btc: string;
+};
 
 type OfferResults = {[keys: string]:string};
-const defaultAmount = '100';
 
+const defaultAmount = '100';
 
 function App() {
   const [prevAmount,setPrevAmount] = useState(defaultAmount);
@@ -44,45 +45,38 @@ function App() {
     }
   }, 300, [amount]);
 
-  const sortedCache = sortBy(cachedResults, 'btc').reverse();
+  const sortedCache:CachedResult[] = sortBy(cachedResults, 'btc').reverse();
   const sortedResults:CachedResult[] = sortBy(Object.keys(offerResults).map(provider => ({
-    provider,
-    btc:offerResults[provider]
-})), 'btc').reverse();
+      provider,
+      btc:offerResults[provider]
+  })), 'btc').reverse();
 
   const showCached = amount === defaultAmount;
 
   const rows = showCached ? sortedCache : sortedResults;
-  
+
   return (
     <main className="max-w-2xl mx-auto px-4 py-8">
-      <h1 className="uppercase text-6xl text-center font-bold
-       bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text
-       text-transparent from-30%">Find cheapest BTC</h1>
-       <div className="flex justify-center mt-4">
-          <AmountInput 
-          value = {amount} 
+      <h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%">Find cheapest BTC</h1>
+      <div className="flex justify-center mt-6">
+        <AmountInput
+          value={amount}
           onChange={e => setAmount(e.target.value)}
           />
       </div>
       <div className="mt-6">
-        { loading && (
-          <>
-        <ResultRow loading={true}  />
-        <ResultRow loading={true}   />
-        <ResultRow loading={true}   />
-        <ResultRow loading={true}   />
-        </>
+        {loading && (
+          <LoadingSkeleton />
         )}
-        {!loading && showCached && sortedCache.map((result:CachedResult) =>  (
-            <ResultRow 
-            providerName={result.provider} 
-            btc = {result.btc}
-            />
+        {!loading && rows.map(result => (
+          <ResultRow
+            key={result.provider}
+            providerName={result.provider}
+            btc={result.btc}
+          />
         ))}
       </div>
     </main>
-     
   )
 }
 
