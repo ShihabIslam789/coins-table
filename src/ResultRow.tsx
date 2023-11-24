542279539a08412371c37d28c0b3a84785d0e4df
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-11-24 14:50:57 -0500

    display BTC numbers from website.

--- src/ResultRow.tsx
+++ src/ResultRow.tsx
@@ -18,24 +18,36 @@ type Logo = {
   
 
 export default function ResultRow({loading, providerName, btc}:ResultRowProps) {
+
+    let url = `http://${providerName}.com`;
+    if (providerName === 'guardian') {
+      url += '/buy-btc';
+    }
     return(
-        <div className= "relative border min-h-[64px]  border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2 overflow-hidden ">
-            <div className= "flex gap-4">
-                {providerName && (
-                    <div>logo</div>
-                )}
-                
-                <div className= "grow"> {providerName || ''}</div>
-                {btc && (
-                <div className = "flex gap-2">
-                    <span className ="text-xl text-purple-200/80</div> {new Intl.NumberFormat('sv-se) </span>
-                    <span className= "text-xl text-purple-300/50">BTC</span> 
-                </div>
-                )}
+        <a
+        href={url}
+        target= "_blank"
+        className="block relative border min-h-[64px] border-white/10 rounded-lg bg-gradient-to-r from-purple-500/10 to-blue-500/10 p-4 my-2 overflow-hidden">
+      <div className="flex gap-4">
+        {providerName && (
+          <div className="grow items-center flex">
+            <img
+              src={logos[providerName].source}
+              className={
+                "h-8 "+(logos[providerName]?.invert ? 'invert' : '')
+              }
+        
+              <div className="flex gap-2">
+              <span className="text-xl text-purple-200/80">
+                { new Intl.NumberFormat('sv-SE', {minimumFractionDigits:8}).format(parseFloat(btc))}
+              </span>
+              <span className="text-xl text-purple-300/50">BTC</span>
             </div>
-            {loading && (
-                <div className= "inset-0 absolute bg-gradient-to-r from transparent via-blue-800/50 to-transparent skeleton-animation" />
-            )}
+          )}
         </div>
+        {loading && (
+          <div className="inset-0 absolute bg-gradient-to-r from-transparent via-blue-800/50 to-transparent skeleton-animation border-t border-white/25" />
+        )}
+      </a>
     );
-}
+  }
