8d86c8b54dfad6a4d82115def6d362c7fd98e8ed
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-11-27 19:18:33 -0500

    small tweaks.

--- src/AmountInput.tsx
+++ src/AmountInput.tsx
@@ -2,10 +2,10 @@ import Input, {InputProps} from "./Input";
 
 export default function AmountInput(props: InputProps) {
     return(
-        <div className= "bg-blue-950 border border-white/10 rounded-lg">
+        <div className= " flex items-center bg-blue-950 border border-white/10 rounded-lg">
             <Input 
             placeholder = "Amount"
-            className = "border-0 w-24"
+            className = "border-0 w-24 pl-4 bg-transparent text-2xl"
             value ={props.value} 
             onChange={props.onChange} />
             <span className= "text-white/50 px-4">USD</span>
